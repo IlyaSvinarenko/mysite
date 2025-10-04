88d11f50a901c94b75c12dc6338311fd448ca43e
IlyaSvinarenko <ilyasvinarenko98007@gmail.com>
2025-10-04 23:53:57 +0300

    Check and fix

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi.exceptions import HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from app.exceptions import TokenExpiredException, TokenNoFoundException
-from app.users.router import router as users_router
+from app.users.router import router as users_router  # Убедитесь, что этот импорт есть
 from app.chat.router import router as chat_router
 
 app = FastAPI()
@@ -18,19 +18,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(users_router)
+app.include_router(users_router)  # И этот include
 app.include_router(chat_router)
-
 
 @app.get("/")
 async def redirect_to_auth():
     return RedirectResponse(url="/auth")
 
-
 @app.exception_handler(TokenExpiredException)
 async def token_expired_exception_handler(request: Request, exc: HTTPException):
     return RedirectResponse(url="/auth")
-
 
 @app.exception_handler(TokenNoFoundException)
 async def token_no_found_exception_handler(request: Request, exc: HTTPException):
